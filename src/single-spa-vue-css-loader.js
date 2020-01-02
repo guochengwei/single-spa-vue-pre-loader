375f6072039a8a9d50bcfdbae8bf63640013a260
guochengwei <guochengwei@360.cn>
2020-01-02 20:19:01 +0800

    babel config, package.json files

--- src/single-spa-vue-css-loader.js
+++ src/single-spa-vue-css-loader.js
@@ -2,12 +2,14 @@ const cssLoader = require('css-loader')
 const getLocalIdent = require('css-loader/lib/getLocalIdent')
 
 module.exports = function (content, map) {
-  this.loaders[0].query = JSON.parse(this.loaders[0].options)
-  this.query.getLocalIdent = function (context, localIdentName, localName, options) {
+  const loader = this.loaders[this.loaderIndex]
+  const query = JSON.parse(loader.query)
+  query.getLocalIdent = function (context, localIdentName, localName, options) {
     if (/^el-/.test(localName)) {
       return localName
     }
     return getLocalIdent.apply(null, arguments)
   }
+  loader.query = query
   return cssLoader.call(this, content, map)
 }
