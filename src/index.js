c33901103dc9ef0c6b9830175647a5ef473de6e1
guochengwei <guochengwei@360.cn>
2020-01-02 20:36:46 +0800

    fix loader query & index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-const SingleSpaVueCssLoader = require('single-spa-vue-css-loader.js')
-const SingleSpaVuePreLoader = require('single-spa-vue-pre-loader.js')
+const SingleSpaVueCssLoader = require('./single-spa-vue-css-loader.js')
+const SingleSpaVuePreLoader = require('./single-spa-vue-pre-loader.js')
+const loaderUtils = require('loader-utils')
 
-module.exports = { SingleSpaVueCssLoader, SingleSpaVuePreLoader }
+module.exports = function (content) {
+  const query = loaderUtils.getOptions(this)
+  if (query.type === 'css') {
+    return SingleSpaVueCssLoader.apply(this,arguments)
+  } else if (query.type === 'vue') {
+    return SingleSpaVuePreLoader.apply(this, arguments)
+  } else {
+    return content
+  }
+}
--- src/single-spa-vue-css-loader.js
+++ src/single-spa-vue-css-loader.js
@@ -1,9 +1,10 @@
 const cssLoader = require('css-loader')
 const getLocalIdent = require('css-loader/lib/getLocalIdent')
+const loaderUtils = require('loader-utils')
 
 module.exports = function (content, map) {
   const loader = this.loaders[this.loaderIndex]
-  const query = JSON.parse(loader.query)
+  const query = loaderUtils.getOptions(this)
   query.getLocalIdent = function (context, localIdentName, localName, options) {
     if (/^el-/.test(localName)) {
       return localName
