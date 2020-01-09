eef41b50cf94b6e752a6e4dc319c8d2c21d3de84
guochengwei <guochengwei@360.cn>
2020-01-09 11:41:35 +0800

    remove css-loader

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-const SingleSpaVueCssLoader = require('./single-spa-vue-css-loader.js')
 const SingleSpaVuePreLoader = require('./single-spa-vue-pre-loader.js')
-const loaderUtils = require('loader-utils')
 
-module.exports = function (content) {
-  const query = loaderUtils.getOptions(this)
-  if (query.type === 'css') {
-    return SingleSpaVueCssLoader.apply(this,arguments)
-  } else if (query.type === 'vue') {
-    return SingleSpaVuePreLoader.apply(this, arguments)
-  } else {
-    return content
-  }
-}
+module.exports = SingleSpaVuePreLoader
