49d63706551c83c1f7c825d4c666a12a177cc401
guochengwei <guochengwei@360.cn>
2020-01-10 10:49:31 +0800

    fix parser

--- src/single-spa-vue-template-parser.js
+++ src/single-spa-vue-template-parser.js
@@ -1,11 +1,12 @@
 const unicodeRegExp = /a-zA-Z\u00B7\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u037D\u037F-\u1FFF\u200C-\u200D\u203F-\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD/
 const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
 const dynamicArgAttribute = /^\s*((?:v-[\w-]+:|@|:|#)\[[^=]+\][^\s"'<>\/=]*)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
-const ncname = '[a-zA-Z_][\\-\\.0-9_a-zA-Z' + (unicodeRegExp.source) + ']*'
+const ncname = '[a-zA-Z_][\\-\\.0-9_a-zA-Z' + unicodeRegExp.source + ']*'
 const qnameCapture = '((?:' + ncname + '\\:)?' + ncname + ')'
-const startTagOpen = new RegExp(('^\\s*<' + qnameCapture))
+const startTagOpen = new RegExp('^\\s*<' + qnameCapture)
 const startTagClose = /^\s*(\/?)>/
 const comment = /^<!\--/
+const endTag = new RegExp(('^<\\/' + qnameCapture + '[^>]*>'))
 
 function parseStartTag (html) {
   let index = 0
@@ -57,12 +58,30 @@ function parseStartTag (html) {
 module.exports = function (context) {
   const stringBuffer = []
   let last = 0
-  let startTagMatch = parseStartTag(context)
+  let html = context
 
-  while (startTagMatch) {
+  while (html) {
+    let textEnd = html.indexOf('<')
+    if (comment.test(html)) {
+      let commentEnd = html.indexOf('-->')
+      if (commentEnd >= 0) {
+        last += (commentEnd + 3)
+        html = html.substring(commentEnd + 3)
+        continue
+      }
+    }
 
-    let staticClassList, dynamicClass
-    if (!comment.test(startTagMatch.context)) {
+    const endTagMatch = html.match(endTag)
+    if (endTagMatch) {
+      stringBuffer.push(html.slice(0, endTagMatch[0].length))
+      last += endTagMatch[0].length
+      html = html.substring(endTagMatch[0].length)
+      continue
+    }
+
+    if (startTagOpen.test(html)) {
+      let staticClassList, dynamicClass
+      let startTagMatch = parseStartTag(html)
       startTagMatch.attrs.forEach(attr => {
         if (attr[1] === 'class') {
           staticClassList = '[\'' + attr[3].split(' ').join('\',\'') + '\']'
@@ -70,47 +89,48 @@ module.exports = function (context) {
           dynamicClass = attr
         }
       })
-    }
-
-    if (dynamicClass) {
-      stringBuffer.push(context.slice(last, last + dynamicClass.start))
-      const codeFrame = staticClassList
-        ? ` :class="$classWrapper(${dynamicClass[3]}).concat($classWrapper(${staticClassList}))"`
-        : ` :class="$classWrapper(${dynamicClass[3]})"`
-      stringBuffer.push(codeFrame)
-      stringBuffer.push(context.slice(last + dynamicClass.end, last + startTagMatch.end))
-    } else if (staticClassList) {
-      if (startTagMatch.unarySlash) {
-        stringBuffer.push(context.slice(last, last + startTagMatch.end - 2))
-        stringBuffer.push(` :class="$classWrapper(${staticClassList})"`)
-        stringBuffer.push('/>')
+      if (dynamicClass) {
+        stringBuffer.push(html.slice(0, dynamicClass.start))
+        const codeFrame = staticClassList ?
+          ` :class="$classWrapper(${dynamicClass[3]}).concat($classWrapper(${staticClassList}))"` :
+          ` :class="$classWrapper(${dynamicClass[3]})"`
+        stringBuffer.push(codeFrame)
+        stringBuffer.push(html.slice(dynamicClass.end, startTagMatch.end))
+      } else if (staticClassList) {
+        if (startTagMatch.unarySlash) {
+          stringBuffer.push(html.slice(0, startTagMatch.end - 2))
+          stringBuffer.push(` :class="$classWrapper(${staticClassList})"`)
+          stringBuffer.push('/>')
+        } else {
+          stringBuffer.push(html.slice(0, startTagMatch.end - 1))
+          stringBuffer.push(` :class="$classWrapper(${staticClassList})"`)
+          stringBuffer.push('>')
+        }
       } else {
-        stringBuffer.push(context.slice(last, last + startTagMatch.end - 1))
-        stringBuffer.push(` :class="$classWrapper(${staticClassList})"`)
-        stringBuffer.push('>')
+        stringBuffer.push(html.slice(0, startTagMatch.end))
       }
-
-    } else {
-      stringBuffer.push(context.slice(last, last + startTagMatch.end))
+      last += startTagMatch.end
+      html = html.substring(startTagMatch.end)
+      continue
     }
-    last += startTagMatch.end
-    let rest = context.slice(last)
-    let textEnd = rest.indexOf('<')
-    while (!startTagOpen.test(rest) &&
-           !comment.test(rest) &&
-           textEnd >= 0) {
-      if (textEnd === 0) {
-        textEnd = rest.indexOf('<', 1)
+
+    let rest, next
+    if (textEnd >= 0) {
+      rest = html.slice(textEnd)
+      while (!startTagOpen.test(rest) && !comment.test(rest) && !endTag.test(rest)) {
+        next = rest.indexOf('<', 1)
+        if (next < 0) { break }
+        textEnd += next
+        rest = html.slice(textEnd)
       }
       last += textEnd
-      stringBuffer.push(rest.slice(0, textEnd))
-      rest = rest.slice(textEnd)
-      textEnd = rest.indexOf('<')
+      stringBuffer.push(html.slice(0, textEnd))
+      html = html.substring(textEnd)
     }
 
-    startTagMatch = parseStartTag(rest)
+    if (textEnd < 0) {
+      break
+    }
   }
-  stringBuffer.push(context.slice(last))
-  stringBuffer.join('')
   return stringBuffer.join('')
 }
